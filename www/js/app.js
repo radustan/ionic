bc5ce63adde455abf976498bae147b1ec53b21af
radustan <raduadrian.stan@gmail.com>
2016-03-11 10:11:44 +0200

    and_1: Implemented android app integration

--- www/js/app.js
+++ www/js/app.js
@@ -5,81 +5,91 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova','starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.home', {
-    url: '/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeCtrl'
-      }
-    }
-  })
-
-  .state('tab.players', {
-      url: '/players',
-      views: {
-        'tab-players': {
-          templateUrl: 'templates/tab-players.html',
-          controller: 'PlayersCtrl'
-        }
-      }
-    })
-    .state('tab.player-details', {
-      url: '/players/:pId',
-      views: {
-        'tab-players': {
-          templateUrl: 'templates/player-details.html',
-          controller: 'PlayerDetailsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .filter('trusted', ['$sce', function ($sce) {
+        return function(url) {
+            return $sce.trustAsResourceUrl(url);
+        };
+    }])
+    
+    .config(function ($compileProvider) {
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-});
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.home', {
+                url: '/home',
+                views: {
+                    'tab-home': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+            })
+
+            .state('tab.players', {
+                url: '/players',
+                views: {
+                    'tab-players': {
+                        templateUrl: 'templates/tab-players.html',
+                        controller: 'PlayersCtrl'
+                    }
+                }
+            })
+            .state('tab.player-details', {
+                url: '/players/:pId',
+                views: {
+                    'tab-players': {
+                        templateUrl: 'templates/player-details.html',
+                        controller: 'PlayerDetailsCtrl'
+                    }
+                }
+            })
+
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/home');
+
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,77 +1,195 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+    .controller('HomeCtrl', function ($scope) {
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        };
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('PlayerDetailsCtrl', function($scope, $http, $stateParams, $sce) {
-    $http.get('js/data.json').success(function(data) {
-      for (var i = 0; i < data.players.length; i++) {
-        if (data.players[i].number === parseInt($stateParams.pId)) {
-          $scope.player = data.players[i];
-          var imgUrl = 'img/' + $scope.player.shortname + '.jpg';
-          $scope.player.img = imgUrl;
-              console.log($scope.player);
-        }
-      }
-    });
-})
-
-.controller('PlayersCtrl', ['$scope', '$http', '$state',
-  function($scope, $http) {
-    $http.get('js/data.json').success(function(data) {
-      $scope.players = data.players;
-      $scope.data = { showDelete: false, showReorder: false };
-
-      $scope.onItemDelete = function(item) {
-        $scope.players.splice($scope.players.indexOf(item), 1);
-      };
-
-      $scope.doRefresh = function() {
-        $http.get('js/data.json').success(function(data) {
-          $scope.players = data.players;
-          $scope.$broadcast('scroll.refreshComplete');
+    .controller('PlayerDetailsCtrl', function ($scope, $http, $stateParams, $sce) {
+        $http.get('js/data.json').success(function (data) {
+            for (var i = 0; i < data.players.length; i++) {
+                if (data.players[i].number === parseInt($stateParams.pId)) {
+                    $scope.player = data.players[i];
+                    var imgUrl = 'img/' + $scope.player.shortname + '.jpg';
+                    $scope.player.img = imgUrl;
+                    console.log($scope.player);
+                }
+            }
         });
-      };
+    })
 
-      $scope.toggleStar = function(item) {
-        item.star = !item.star;
-      };
+    .controller('PlayersCtrl', ['$scope', '$http', '$state',
+        function ($scope, $http) {
+            $http.get('js/data.json').success(function (data) {
+                $scope.players = data.players;
+                $scope.data = {showDelete: false, showReorder: false};
 
-      $scope.moveItem = function(item, fromIndex, toIndex) {
-        $scope.players.splice(fromIndex, 1);
-        $scope.players.splice(toIndex, 0, item);
-      };
+                $scope.onItemDelete = function (item) {
+                    $scope.players.splice($scope.players.indexOf(item), 1);
+                };
+
+                $scope.doRefresh = function () {
+                    $http.get('js/data.json').success(function (data) {
+                        $scope.players = data.players;
+                        $scope.$broadcast('scroll.refreshComplete');
+                    });
+                };
+
+                $scope.toggleStar = function (item) {
+                    item.star = !item.star;
+                };
+
+                $scope.moveItem = function (item, fromIndex, toIndex) {
+                    $scope.players.splice(fromIndex, 1);
+                    $scope.players.splice(toIndex, 0, item);
+                };
+            });
+        }])
+
+    .controller('AccountCtrl', function ($scope, Camera, $cordovaFile, $ionicModal, $cordovaSms, $ionicLoading) {
+        $scope.settings = {
+            enableFriends: true
+        };
+        $scope.zoomMin = 1;
+        $scope.images = [];
+        $scope.getPhoto = function () {
+            Camera.getPicture().then(function (imageURI) {
+
+                $scope.lastPhoto = imageURI;
+                onImageSuccess(imageURI);
+
+                function onImageSuccess(fileURI) {
+                    createFileEntry(fileURI);
+                }
+
+                function createFileEntry(fileURI) {
+                    window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+                }
+
+                // 5
+                function copyFile(fileEntry) {
+                    var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+                    var newName = makeid() + name;
+                    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+                            fileEntry.copyTo(
+                                fileSystem2,
+                                newName,
+                                onCopySuccess,
+                                fail
+                            );
+                        },
+                        fail);
+                }
+
+                // 6
+                function onCopySuccess(entry) {
+                    $scope.$apply(function () {
+                        console.log(entry.nativeURL);
+                        $scope.images.push(entry.nativeURL);
+                    });
+                }
+
+                function fail(error) {
+                    console.log("fail: " + error.code);
+                }
+
+                function makeid() {
+                    var text = "";
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                    for (var i=0; i < 5; i++) {
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
+                    }
+                    return text;
+                }
+            }, function (err) {
+                console.err(err);
+            }, {
+                quality: 75,
+                targetWidth: 320,
+                targetHeight: 320,
+                saveToPhotoAlbum: false,
+                destinationType : 1,
+                sourceType : 1, // Camera.PictureSourceType.PHOTOLIBRARY
+                allowEdit : false,
+                encodingType: 0
+            });
+        };
+        $scope.urlForImage = function(imageName) {
+            var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+            var trueOrigin = cordova.file.dataDirectory + name;
+            return trueOrigin;
+        };
+
+
+        $scope.showImages = function(index) {
+            $scope.activeSlide = index;
+            $scope.showModal('templates/image-popover.html');
+        };
+
+        $scope.showModal = function(templateUrl) {
+            $ionicModal.fromTemplateUrl(templateUrl, {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function(modal) {
+                $scope.modal = modal;
+                $scope.modal.show();
+            });
+        };
+
+        // Close the modal
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+            $scope.modal.remove()
+        };
+
+        $scope.updateSlideStatus = function(slide) {
+            var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
+            if (zoomFactor == $scope.zoomMin) {
+                $ionicSlideBoxDelegate.enableSlide(true);
+            } else {
+                $ionicSlideBoxDelegate.enableSlide(false);
+            }
+        };
+        
+        $scope.smsresponse = ''
+
+        $scope.sendSMS = function(number, text) {
+            if (!isNaN(number) && text) {
+                $ionicLoading.show({
+                    content: 'Loading',
+                    animation: 'fade-in',
+                    showBackdrop: true,
+                    maxWidth: 200,
+                    showDelay: 0
+                });
+                $cordovaSms
+                    .send(number, text, [])
+                    .then(function() {
+                        $scope.smsresponse = 'Message Sent';
+                        $ionicLoading.hide();
+                    }, function(error) {
+                        $scope.smsresponse = 'Message Not Sent';
+                        $ionicLoading.hide();
+                    });
+
+            }
+        }
     });
-  }])
-
-.controller('AccountCtrl', function($scope, Camera) {
-  $scope.settings = {
-    enableFriends: true
-  };
-
-  $scope.getPhoto = function() {
-    Camera.getPicture().then(function (imageURI) {
-      console.log(imageURI);
-    }, function (err) {
-      console.err(err);
-    });
-  }
-});
